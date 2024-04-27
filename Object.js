081f307f086814ae046ddad7a2e67b09d630df83
redwan9011 <redwan390309@gmail.com>
2024-04-27 23:44:20 +0600

    add all js file

---
+++ Object.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'redwan',
+    age: 22,
+
+}
+
+console.log(person.name);
+console.log(person['age']);
+
---
+++ function.js
@@ -0,0 +1,32 @@
+function  sleep( ) {
+console.log('redwan  is sleeping');
+}
+sleep()    
+console.log('my name is redwan');
+
+
+function name( name) {
+        console.log( 'his name is' , name);
+}
+name('redwan')
+name('sohan')
+
+
+
+// Function is called, the return value will end up in x
+let x = myFunction(4, 3);
+console.log(x);
+function myFunction(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}
+
+
+/* function myFunction(a, b) {
+
+  return a * b;
+}
+
+let y = myFunction(4, 5)
+
+console.log(y);  */
