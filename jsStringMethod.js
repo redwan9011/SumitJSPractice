5a8db72fe9785fc8694a6751a726999b8155a9b9
redwan9011 <redwan390309@gmail.com>
2024-05-16 10:30:30 +0600

    practicing js string method

---
+++ jsStringMethod.js
@@ -0,0 +1,10 @@
+let name = "redwan, rayhan, sohan"
+let rayhan = name.slice(14,21);
+// slice 14- 21 er element nibe , - index oo  nite pare, like (-12, -6),, sudhu ekta value dile (5), tahole er porer sob value asbe
+let sohan = name.substring(5, 14)
+// substrin ar slice same, but substring a negative deya jayna
+
+let redwan = name.substr(8, 6)
+//  first parameter start point, ar second parameter koyta value nibe seta bujhay
+
+console.log(redwan);
